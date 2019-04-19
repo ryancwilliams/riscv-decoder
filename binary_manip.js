02e09ccde9b40c0d5bc5dee057b6e269564d4591
ryancwilliams <rcw4rcw@gmail.com>
2019-04-19 01:12:24 -0400

    Created a way to work with binary numbers.

---
+++ binary_manip.js
@@ -0,0 +1,64 @@
+/**
+ * A library for binary manipulation.
+ * 
+ * @author ryancwilliams
+ */
+
+/**
+ * A binary number
+ */
+class binary {
+
+    /**
+     * The number as a string of bits
+     */
+    bits;
+
+    /**
+     * Gets this binary as a number. 
+     */
+    getAsNumber() {
+        let num = Number.parseInt(this.bits, 2);
+        return num;
+    }
+}
+
+/**
+ * Creates a new binary from a Number object.
+ * 
+ * @param {*} num the Number object.
+ */
+function binaryFromNumber(num) {
+
+    Console.assert(num instanceof Number);
+
+    let binary = new binary();
+    binary.bits = num.toString(2);
+    return binary;
+}
+
+/**
+ * Creates a new binary from a hex string.
+ * 
+ * @param {*} hex The hex string.
+ */
+function binaryFromHex(hex) {
+    let num = Number.parseInt(hex, 16);
+
+    Console.assert(!Number.isNaN(num));
+
+    return binaryFromNumber(num);
+}
+
+/**
+ * Creates a new binary from a decimal string.
+ * 
+ * @param {*} hex The decimal string.
+ */
+function binaryFromHex(dec) {
+    let num = Number.parseInt(dec, 10);
+
+    Console.assert(!Number.isNaN(num));
+
+    return binaryFromNumber(num);
+}

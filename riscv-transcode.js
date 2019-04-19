49a14c54f15f9344e8f41cd94e92056b54266d3f
ryancwilliams <rcw4rcw@gmail.com>
2019-04-19 19:16:15 -0400

    Added Opcode Structure
    For #4

--- riscv-transcode.js
+++ riscv-transcode.js
@@ -76,4 +76,47 @@ const RV_INSTYPE = {
     J_TYPE: new RiscvInsType("J-type"),
     OTHER_TYPE: new RiscvInsType("Other-Type")
 }
+
+/**
+ * A Riscv Opcode
+ */
+class RiscvOpcode {
+
+    /**
+     * The opcode for the instruction.
+     */
+    opcode;
+
+    /**
+     * The instruction type.
+     */
+    instype;
+
+    /**
+     * Creates a new Riscv Opcode
+     * @param {*} opcode The Opcode as a number
+     * @param {*} instype The instruction type
+     */
+    constructor(opcode, instype){
+        this.opcode = opcode;
+        this.instype = instype;
+    }
+
+}
+
+/**
+ * The Opcodes in RiscV
+ */
+const RV_OPCODES = {
+    LOAD: new RiscvOpcode(Number.parseInt("0000011", 2), RV_INSTYPE.I_TYPE),
+    STORE: new RiscvOpcode(Number.parseInt("0100011", 2), RV_INSTYPE.S_TYPE),
+    BRANCH: new RiscvOpcode(Number.parseInt("1100011", 2), RV_INSTYPE.B_TYPE),
+    JALR: new RiscvOpcode(Number.parseInt("1100111", 2), RV_INSTYPE.I_TYPE),
+    MISC_MEM: new RiscvOpcode(Number.parseInt("0001111", 2), RV_INSTYPE.OTHER_TYPE),
+    JAL: new RiscvOpcode(Number.parseInt("1101111", 2), RV_INSTYPE.J_TYPE),
+    OP_IMM: new RiscvOpcode(Number.parseInt("0010011", 2), RV_INSTYPE.I_TYPE),
+    OP: new RiscvOpcode(Number.parseInt("0110011", 2), RV_INSTYPE.R_TYPE),
+    SYSTEM: new RiscvOpcode(Number.parseInt("1110011", 2), RV_INSTYPE.OTHER_TYPE),
+    AUIPC: new RiscvOpcode(Number.parseInt("0010111", 2), RV_INSTYPE.U_TYPE),
+    LUI: new RiscvOpcode(Number.parseInt("0110111", 2), RV_INSTYPE.U_TYPE),
 }
